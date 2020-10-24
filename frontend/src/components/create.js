b9b2f96ec8ef3823fdfe4566558c135aa515de82
Keola Morse <keolamorse@gmail.com>
2020-10-23 23:52:54 -0400

    styled components

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import '../componentCSS/create.css';
+import styled from 'styled-components';
+
+const CreateForm = styled.form`
+    color: red;    
+     text-align: center;
+` ;
 
 const AddNewTask = () => {
 
@@ -14,55 +20,57 @@ const AddNewTask = () => {
     })
 
     const onChangeTask = (e) => {
-        setValues({...newTask, task_job: e.target.value});
+        setValues({ ...newTask, task_job: e.target.value });
     }
     const onChangeAddedBy = (e) => {
-        setValues({...newTask, added_by: e.target.value});
+        setValues({ ...newTask, added_by: e.target.value });
     }
     const onChangeNotes = (e) => {
-        setValues({...newTask, notes: e.target.value});
+        setValues({ ...newTask, notes: e.target.value });
     }
     const onSubmit = (e) => {
         e.preventDefault();
         axios.post('http://localhost:4000/tasks', newTask)
-        .then(res => console.log(res.data))
-        .then(() => setValues(() => ({
+            .then(res => console.log(res.data))
+            .then(() => setValues(() => ({
                 task_job: '',
                 added_by: '',
                 notes: '',
                 task_status: 0,
                 returnToList: true
-                })
+            })
             )
-        ) 
-        .catch(error => console.log(error)
-        )  
+            )
+            .catch(error => console.log(error)
+            )
     }
-    
-    if(newTask.returnToList === true){
+
+    if (newTask.returnToList === true) {
         return <Redirect to='/' />
     }
     return (
-        <div id="create-form">
-            <p>Add new Task</p>
-            <form onSubmit={onSubmit} id="submit-cells">
-                <div className="form-group">
-                    <label>Task: </label>
-                    <input type="text" className="form-control" value={newTask.task_job} onChange={onChangeTask }/>
-                </div>
-                <div className="form-group">
-                <label>Added By: </label>
-                    <input type="text" className="form-control" value={newTask.added_by} onChange={onChangeAddedBy }/>
-                </div>
-                <div className="form-group">
-                <label>Details/Notes: </label>
-                    <input type="text" className="form-control" value={newTask.notes} onChange={onChangeNotes }/>
-                </div>
-                <div className="form-group">
-                    <input type="submit" value="Add Task" className="btn btn-primary"/>
-                </div>
-            </form>
-        </div>
+        <CreateForm>
+            <div id="create-form">
+                <p>Add new Task</p>
+                <form onSubmit={onSubmit} id="submit-cells">
+                    <div className="form-group">
+                        <label>Task: </label>
+                        <input type="text" className="form-control" value={newTask.task_job} onChange={onChangeTask} />
+                    </div>
+                    <div className="form-group">
+                        <label>Added By: </label>
+                        <input type="text" className="form-control" value={newTask.added_by} onChange={onChangeAddedBy} />
+                    </div>
+                    <div className="form-group">
+                        <label>Details/Notes: </label>
+                        <input type="text" className="form-control" value={newTask.notes} onChange={onChangeNotes} />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Add Task" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </CreateForm>
     )
 }
 
--- server.js
+++ server.js
@@ -17,17 +17,14 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/tasks', tasks);
-
 // if(process.env.NODE_ENV === 'development') {
 //     app.use(morgan('dev'));
 // }
-
 // if(process.env.NODE_ENV === 'production') {
 //     app.use(express.static('client/build'));
 
 //     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 // }
-
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function () {
