cde17ff854c203f736c11e5d76d0df6a0377deee
Keola Morse <keolamorse@gmail.com>
2020-12-07 23:56:29 -0500

    update css

--- frontend/src/services/VerifyUser.js
+++ frontend/src/services/VerifyUser.js
@@ -2,27 +2,33 @@
 import axios from 'axios';
 
 const verifyLogin = async () => {
-  let currentUser = sessionStorage.getItem('user');
-  if (currentUser) {   
-      let webToken = JSON.parse(currentUser).token;
-      try {
-          const user = await axios.get('/api/users/profile',
-          {
-              headers: {
-                  'Content-type': 'application/json',
-                  'authorization': `Bearer ${webToken}`
-              }
-          })
-          return user.data;
-      }
-      
-      catch (error) {
-          console.log(error)
-      }
-  } else {
-      console.log("Logged in as guest");
-      return;
-  }
+
+    let currentUser = sessionStorage.getItem('user');
+    if (currentUser) {   
+
+        let webToken = JSON.parse(currentUser).token;
+        try {
+
+            const user = await axios.get('/api/users/profile',
+            {
+                     
+                headers: {
+                    'Content-type': 'application/json',
+                    'authorization': `Bearer ${webToken}`
+                }
+            })
+            return user.data;
+
+        }
+        catch (error) {s
+            console.log(error)
+        }
+    } else {
+
+      sessionStorage.clear();
+    }
+    console.log("Please Login");
+    return; 
 }
 
 
